89e31374f3d424f8b27bcbbbf9dddeea643774f9
dankeboy36 <dankeboy36@gmail.com>
2025-01-21 17:39:11 +0100

    fix: correct `files` declaration in `package.json`
    Publish the type definition for the lib
    
    Signed-off-by: dankeboy36 <dankeboy36@gmail.com>

---
+++ src/index.d.ts
@@ -0,0 +1,25 @@
+const arduinoTools = [
+  'arduino-cli',
+  'arduino-language-server',
+  'arduino-fwuploader',
+] as const
+const clangTools = ['clangd', 'clang-format'] as const
+
+export const tools = [...arduinoTools, ...clangTools]
+
+export declare type Tool = typeof tools[number]
+
+export declare interface GetToolParams {
+  tool: Tool
+  version: string
+  destinationFolderPath: string
+  platform?: NodeJS.Platform
+  arch?: NodeJS.Architecture
+  force?: boolean
+}
+
+export declare interface GetToolResult {
+  toolPath: string
+}
+
+export declare function getTool(params: GetToolParams): Promise<GetToolResult>
