0fc14bf6895015ff42bd56dcc11e191262d59925
dankeboy36 <111981763+dankeboy36@users.noreply.github.com>
2026-07-16 05:56:25 +0000

    fix: update pinned latest versions

--- src/versions.js
+++ src/versions.js
@@ -1,11 +1,11 @@
 /** @type {Record<import('./index.js').Tool, string>} */
 const latestVersions = {
-  'arduino-cli': '1.4.0',
+  'arduino-cli': '1.5.1',
   'arduino-language-server': '0.7.7',
   'arduino-fwuploader': '2.4.1',
   'arduino-lint': '1.3.0',
-  clangd: '15.0.0',
-  'clang-format': '15.0.0',
+  clangd: '22.1.8',
+  'clang-format': '22.1.8',
 }
 
 export default {
